120d263e73527376d5b3b063f99756f2469a4376
RajK-NeetiAI <raj@neeti.ai>
2024-02-16 18:43:51 +0530

    success form submit, image on cloud storage

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,19 +3,55 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import fs from "fs";
+import os from "os";
+import path from "path";
+import { v4 as uuidv4 } from 'uuid';
+import { Storage } from '@google-cloud/storage';
+
+import prisma from "@/lib/db";
+
+const keyFilename = 'emerald-griffin-407812-d6735d932119.json';
+const bucketName = 'rajk-neeti-test';
+
+const storage = new Storage({ keyFilename });
+
+export const uploadImage = async (filePath: string, destFileName: string) => {
+    await storage.bucket(bucketName).upload(filePath, {
+        destination: destFileName,
+        predefinedAcl: 'publicRead'
+    });
+    const publicUrl = `https://storage.googleapis.com/${bucketName}/${destFileName}`;
+    return publicUrl;
+};
 
 export const submitReceipeForm = async (formData: FormData) => {
     const parsedFormData = {
-        author: formData.get('author'),
-        email: formData.get('email'),
+        author: <string>formData.get('author'),
+        email: <string>formData.get('email'),
         image: <File>formData.get('image'),
-        instructions: formData.get('instructions'),
-        receipeName: formData.get('receipeName'),
-        summary: formData.get('summary')
+        instructions: <string>formData.get('instructions'),
+        receipeName: <string>formData.get('receipeName'),
+        summary: <string>formData.get('summary')
     };
     const arrayBuffer = await parsedFormData.image.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
-    fs.writeFileSync('sample.jpg', buffer);
+    const filePath = path.join(
+        os.tmpdir(),
+        `${uuidv4()}.jpg`
+    );
+    const destFileName = `${uuidv4()}.jpg`;
+    fs.writeFileSync(filePath, buffer);
+    const publicUrl = await uploadImage(filePath, destFileName);
+    await prisma.post.create({
+        data: {
+            creator: parsedFormData.author,
+            creatorEmail: parsedFormData.email,
+            image: publicUrl,
+            instructions: parsedFormData.instructions,
+            summary: parsedFormData.summary,
+            title: parsedFormData.receipeName
+        }
+    });
     revalidatePath('/meal');
     redirect('/meal');
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                port: ''
+            }
+        ]
+    },
+    compiler: {
+        removeConsole: true
+    },
+    swcMinify: true
+};
 
 export default nextConfig;

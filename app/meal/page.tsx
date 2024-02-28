54d4eb965b11e8019875cb41e29097611c5c004e
RajK-NeetiAI <raj@neeti.ai>
2024-02-28 18:12:34 +0530

    fixed brows meals layout

--- app/meal/page.tsx
+++ app/meal/page.tsx
@@ -8,29 +8,31 @@ export default async function MealPage() {
     const mealIteams = await fetchAllMealItems();
     const shuffledMealIteams = mealIteams.sort(() => Math.random() - 0.5);
     return (
-        <div className="flex flex-row gap-4 text-center w-full">
-            {
-                Array.from(shuffledMealIteams).map((meal, index) => (
-                    <div key={index}>
-                        <Card className="">
-                            <CardHeader>
-                                <CardTitle className="font-bold">{meal.title}</CardTitle>
-                                <CardDescription className="font-semibold justify-normal">{meal.summary}</CardDescription>
-                            </CardHeader>
-                            <CardContent>
-                                <Image src={meal.image} alt={meal.title} width={256} height={256}></Image>
-                            </CardContent>
-                            <CardFooter className="justify-end">
-                                <Link className="text-blue-500" href={`/meal/${meal.id}`}>
-                                    <div className="bg-slate-600 p-2 rounded-md text-white hover:bg-slate-300 hover:text-black font-extrabold">
-                                        Read more...
-                                    </div>
-                                </Link>
-                            </CardFooter>
-                        </Card>
-                    </div>
-                ))
-            }
+        <div className="flex flex-col items-center justify-center m-2">
+            <div className="grid grid-cols-3 gap-2 m-2">
+                {
+                    Array.from(shuffledMealIteams).map((meal, index) => (
+                        <div key={index}>
+                            <Card className="">
+                                <CardHeader>
+                                    <CardTitle className="font-bold">{meal.title}</CardTitle>
+                                    <CardDescription className="font-semibold justify-normal">{meal.summary}</CardDescription>
+                                </CardHeader>
+                                <CardContent className="flex items-center justify-center">
+                                    <Image className="rounded-lg" src={meal.image} alt={meal.title} width={256} height={256}></Image>
+                                </CardContent>
+                                <CardFooter className="justify-end">
+                                    <Link className="text-blue-500" href={`/meal/${meal.id}`}>
+                                        <div className="bg-slate-600 p-2 rounded-md text-white hover:bg-slate-300 hover:text-black font-extrabold">
+                                            Read more...
+                                        </div>
+                                    </Link>
+                                </CardFooter>
+                            </Card>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
